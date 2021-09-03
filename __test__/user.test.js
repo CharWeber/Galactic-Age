7f1ba1f82f1517b8cc34e970ef797e370d0a6abe
Charlie Weber <charlestweber@gmail.com>
2021-09-03 08:56:51 -0700

    add test #2 mercury. Good fail. RED PHASE

--- __test__/user.test.js
+++ __test__/user.test.js
@@ -2,10 +2,18 @@ import User from './../src/js/user.js'
 
 describe("User", () => {
 
+  let newUser;
+
+  beforeEach(() => {
+  newUser = new User("Steven", "45")
+  })
 
   test('should corretly generate user with age', () => {
-    let newUser = new User("Steven", "45")
     expect(newUser.name).toEqual('Steven')
     expect(newUser.age).toEqual(45)
+  })
+
+  test('should correctly determine age on mercury {}.24 earth years}', () =>{
+    expect(newUser.mercuryAge).toEqual(10.8)
   })
 })
